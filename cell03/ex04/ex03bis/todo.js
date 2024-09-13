c6ab09bcbe9887b134937f386dc5b87760ec47a1
PonChuenjitsiri <111419266+PonChuenjitsiri@users.noreply.github.com>
2024-09-13 19:53:08 +0700

    Refactor background.html and backgroound.css files

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,46 @@
+$(function () {
+  loadTodoList();
+
+  $("#new_task").click(function () {
+    const task = prompt("Enter a new TO DO:");
+    if (task) {
+      addTodo(task);
+      saveTodoList();
+    }
+  });
+
+  function addTodo(task) {
+    const todoItem = $("<div></div>")
+      .addClass("todo-item")
+      .text(task)
+      .on("click", function () {
+        const confirmDelete = confirm("Do you want to remove this TO DO?");
+        if (confirmDelete) {
+          $(this).remove();
+          saveTodoList();
+        }
+      });
+
+    $("#ft_list").prepend(todoItem);
+  }
+
+  function saveTodoList() {
+    const todos = [];
+    $(".todo-item").each(function () {
+      todos.push($(this).text());
+    });
+    document.cookie = `todos=${JSON.stringify(todos)}; path=/`;
+  }
+
+  function loadTodoList() {
+    const cookies = document.cookie.split(";");
+    const todoCookie = cookies.find((cookie) =>
+      cookie.trim().startsWith("todos=")
+    );
+
+    if (todoCookie) {
+      const todoList = JSON.parse(todoCookie.split("=")[1]);
+      todoList.forEach((task) => addTodo(task));
+    }
+  }
+});
