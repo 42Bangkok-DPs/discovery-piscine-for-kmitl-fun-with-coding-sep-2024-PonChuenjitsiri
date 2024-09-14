bb0260aafd8bbbe4fced91013db7c3486fd0b7db
PonChuenjitsiri <111419266+PonChuenjitsiri@users.noreply.github.com>
2024-09-14 12:01:25 +0700

    Refactor header styling and make it fixed at the top of the page

--- rush/main/home.js
+++ rush/main/home.js
@@ -1,53 +1,52 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        screens: {
-             'sm': '390px',
-             'md': '960px',
-             'lg': '1440px',
-        },
-      }
+  theme: {
+    extend: {
+      screens: {
+        'sm': '390px',
+        'md': '960px',
+        'lg': '1440px',
+      },
     }
   }
+}
 
-   function navigateToPage(path) {
-       window.location.href = path;
-   }
+function navigateToPage(path) {
+  window.location.href = path;
+}
 document.addEventListener('DOMContentLoaded', () => {
-const cardPon = document.getElementById('pon-card');
-const img = document.getElementById('Pon');
-const OakImg = document.getElementById('Oak')
-const cardOak = document.getElementById('oak-card')
-const BgCNX = document.getElementById('cnx')
-if (cardPon && img) { // Ensure both elements exist
-  cardPon.addEventListener('mouseover', () => {
-    img.classList.remove('invisible');
-    img.classList.add('showImage')
-    
-  });
+  const cardPon = document.getElementById('pon-card');
+  const img = document.getElementById('Pon');
+  const OakImg = document.getElementById('Oak')
+  const cardOak = document.getElementById('oak-card')
+  const BgCNX = document.getElementById('cnx')
+  if (cardPon && img) { // Ensure both elements exist
+    cardPon.addEventListener('mouseover', () => {
+      img.classList.remove('invisible');
+      img.classList.add('showImage')
 
-  cardPon.addEventListener('mouseout', () => {
-    img.classList.add('invisible');
-    img.classList.remove('showImage');
-   
-  });
-} 
-else {
-  console.error('Element(s) not found');
-}
-if (OakImg&&cardOak){
-   cardOak.addEventListener('mouseover', () => {
-    OakImg.classList.remove('invisible');
-    OakImg.classList.add('showImage')
-  });
+    });
 
-  cardOak.addEventListener('mouseout', () => {
-    OakImg.classList.add('invisible');
-    OakImg.classList.remove('showImage');
-  });
-}
-else {
-  console.error('Element(s) not found');
-}
+    cardPon.addEventListener('mouseout', () => {
+      img.classList.add('invisible');
+      img.classList.remove('showImage');
+
+    });
+  }
+  else {
+    console.error('Element(s) not found');
+  }
+  if (OakImg && cardOak) {
+    cardOak.addEventListener('mouseover', () => {
+      OakImg.classList.remove('invisible');
+      OakImg.classList.add('showImage')
+    });
+
+    cardOak.addEventListener('mouseout', () => {
+      OakImg.classList.add('invisible');
+      OakImg.classList.remove('showImage');
+    });
+  }
+  else {
+    console.error('Element(s) not found');
+  }
 });
-   
